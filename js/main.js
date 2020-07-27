057d1bb498f43f5c4ca3798c19dffb5cef59a6f8
python <python@ubuntu.ubuntu-domain>
2020-07-28 00:12:46 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// footer date
+
+const footerP = document.getElementById("footerP");
+
+footerP.innerHTML = `Eureka team &copy; ${new Date().getFullYear}`;
+
+// scroll spy
+var spy = new Gumshoe(".text a");
